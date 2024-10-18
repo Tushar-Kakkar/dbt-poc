3cfde2c922cb2af66c1034894989d5ff938c5bfd
tkakkar@salesforce.com <tkakkar@salesforce.com>
2024-10-18 04:13:05 +0000

    commit

--- models/MARTS/FACT_QTY.sql
+++ models/MARTS/FACT_QTY.sql
@@ -1,8 +1,9 @@
 SELECT
     w.w_warehouse_name,
     i.i_category,
+    iv.INV_WAREHOUSE_SK_NEW,
     SUM(iv.inv_quantity_on_hand) AS total_stock
-FROM {{ source('Snowflake_POC', 'inventory') }} iv
+FROM {{ ref('STG_Snowflake_POC_Inventory') }} iv
 JOIN {{ source('Snowflake_POC', 'item') }} i ON iv.inv_item_sk = i.i_item_sk
 JOIN {{ source('Snowflake_POC', 'warehouse') }} w ON iv.inv_warehouse_sk = w.w_warehouse_sk
-GROUP BY w.w_warehouse_name, i.i_category 
+GROUP BY w.w_warehouse_name, i.i_category ,iv.INV_WAREHOUSE_SK_NEW

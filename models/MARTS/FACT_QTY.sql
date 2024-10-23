7107a1f4493a7c77ed687aa82a80787ceae844a4
tkakkar@salesforce.com <tkakkar@salesforce.com>
2024-10-23 04:46:10 +0000

    commit

--- models/MARTS/FACT_QTY.sql
+++ models/MARTS/FACT_QTY.sql
@@ -1,14 +1,12 @@
 {% set query_to_run %}
-select distinct i_category from {{ ref('STG_Snowflake_POC_Item') }}
+select distinct i_size from {{ ref('STG_Snowflake_POC_Item') }}
 {% endset %}
 
 {% set results = run_query(query_to_run) %}
 
 {% if execute %}
-   {% set item_categories= results.columns[0].values() %}
-   {{item_categories}}
+   {% set item_sizes= results.columns[0].values() %}
 {% endif %}
-
 
 
 with 
@@ -37,7 +35,16 @@ SELECT
     w.w_warehouse_name,
     i.i_category,
     iv.INV_WAREHOUSE_SK_NEW,
-    SUM(iv.inv_quantity_on_hand) AS total_stock
+    SUM(iv.inv_quantity_on_hand) AS total_stock,
+    {% for size in item_sizes %}
+        {% if size == 'N/A' %}
+            sum(case when i_size = 'N/A' then iv.inv_quantity_on_hand end) as total_stock_NA,
+        {% elif size == 'extra large' %}
+            sum(case when i_size = 'extra large' then iv.inv_quantity_on_hand end) as total_stock_extra_large,
+        {% else %}
+            sum(case when i.i_size = '{{ size }}' then iv.inv_quantity_on_hand end) as total_stock_{{size}},
+        {% endif %}
+    {% endfor %}
 FROM inv iv
 JOIN itm i ON iv.inv_item_sk = i.i_item_sk
 JOIN wh w ON iv.inv_warehouse_sk = w.w_warehouse_sk
